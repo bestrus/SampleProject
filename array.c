0eba3a9ae0fb42a2eb179e3de431c204391d4729
Admin Root <admin@admin.com>
2024-03-08 10:12:01 +0000

    fix array

--- array.c
+++ array.c
@@ -1,17 +1,29 @@
 #include <stdio.h>
+
 #define MAX 16
 
-int main(void) {
+int main(void)
+{
    int i, n, arr[MAX];
+
    printf("Enter the total number of elements: ");
    scanf("%d", &n);
 
+   /* handle array overflow condition */
+   if (n > MAX) {
+      fprintf(stderr, "Number of elements must be less than %d\n", MAX);
+      return 1;
+   }
+
    printf("Enter the elements\n");
 
-   for (i = 0; i < n; ++i) scanf("%d", &arr[i]);
+   for (i = 0; i < n; ++i)
+      scanf("%d", &arr[i]);
+
    printf("Array has following elements\n");
-   for (i = 0; i < n; ++i) printf("|%d| ", arr[i]);
-   
-   printf("\n");
+   for (i = 0; i < n; ++i)
+      printf("|%d| ", arr[i]);
+      printf("\n");
+
    return 0;
 }
